c1740913b6f2bb26573db4916f85363148ed1cd5
tral909 <indorm1992@mail.ru>
2019-03-29 00:58:43 +0300

    1.2.9 ngstyle anf ngclass directives

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,12 +9,7 @@ export class CarsComponent {
 
     carName = '';
     addCarStatus = false;
-    cars = ['Ford', 'Audi', 'BMW'];
-    items = [
-        {id: 3, name: 'item 1'},
-        {id: 6, name: 'item 2'},
-        {id: 9, name: 'item 3'}
-    ];
+    cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
 
     constructor() {
     }
@@ -23,5 +18,9 @@ export class CarsComponent {
        this.addCarStatus = true;
        this.cars.push(this.carName);
        this.carName = '';
+    }
+
+    setBigCarText(car: string) {
+        return car.length > 4 ? true : false;
     }
 }
