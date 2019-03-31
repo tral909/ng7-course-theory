ee0627f5717b45ff6dae4872b7f4652d7ee89f17
tral909 <indorm1992@mail.ru>
2019-03-31 23:34:21 +0300

    1.4.2 use renderer in directive

--- src/app4/directives/background.directive.ts
+++ src/app4/directives/background.directive.ts
@@ -1,12 +1,14 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[app4Background]'
 })
 export class BackgroundDirective implements OnInit {
-    constructor(private element: ElementRef) {}
+    constructor(private element: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit() {
-        this.element.nativeElement.style.backgroundColor = 'red';
+        const {nativeElement} = this.element;
+        this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+        this.renderer.addClass(nativeElement, 'white-text');
     }
 }
