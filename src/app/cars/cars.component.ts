45469e5a5b78371b06e1b1663fad62675e562df7
tral909 <indorm1992@mail.ru>
2019-03-31 16:09:16 +0300

    1.3.1 pass param to comp. begin

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,23 +7,30 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
-    carName = '';
-    addCarStatus = false;
-    cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
-    dates = [
-      new Date(2015, 3, 4).toDateString(),
-      new Date(2011, 2, 5).toDateString(),
-      new Date(2017, 4, 6).toDateString(),
-      new Date(2000, 1, 7).toDateString()
-    ];
+  carName = '';
+  carYear = 2017;
 
-    constructor() {
-    }
+  cars: [{name: string, year: number}] = [{
+    name: 'Ford',
+    year: 2015
+  }, {
+    name: 'Mazda',
+    year: 2010
+  }, {
+    name: 'Audi',
+    year: 2017
+  }];
 
-    addCar() {
-       this.addCarStatus = true;
-       this.cars.push(this.carName);
-       this.carName = '';
-    }
+  constructor() {
+  }
+
+  addCar() {
+    this.cars.push({
+      name: this.carName,
+      year: this.carYear
+    });
+    this.carName = '';
+    this.carYear = 2017;
+  }
 
 }
