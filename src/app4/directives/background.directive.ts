2e38868828c4ca1d54eeabb570afbdd6291893d2
tral909 <indorm1992@mail.ru>
2019-04-01 00:24:42 +0300

    1.4.4 pass parameters to directive

--- src/app4/directives/background.directive.ts
+++ src/app4/directives/background.directive.ts
@@ -4,31 +4,35 @@ import {
     OnInit,
     Renderer2,
     HostListener,
-    HostBinding
+    HostBinding,
+    Input
 } from '@angular/core';
 
 @Directive({
     selector: '[app4Background]'
 })
 export class BackgroundDirective implements OnInit {
-
+    @Input('app4Background') hoverColor: string = 'green';
+    @Input() defaultColor: string = 'transparent';
     @HostBinding('style.backgroundColor') background: string;
 
     constructor(/*private element: ElementRef, private renderer: Renderer2*/) {}
 
     ngOnInit() {
-        
+        this.background = this.defaultColor;
     }
 
     @HostListener('mouseenter') mouseEnter() {
         //const {nativeElement} = this.element;
         //this.renderer.setStyle(nativeElement, 'background-color', 'red');
         //this.renderer.removeClass(nativeElement, 'white-text');
-        this.background = 'green';
+        //this.background = 'green';
+        this.background = this.hoverColor;
     }
 
     @HostListener('mouseleave') mouseLeave() {
         //this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
-        this.background = 'transparent';
+        //this.background = 'transparent';
+        this.background = this.defaultColor;
     }
 }
