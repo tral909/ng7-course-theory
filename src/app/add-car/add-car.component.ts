b17487802c3c483cf8b5378d8e4238457d932f87
tral909 <indorm1992@mail.ru>
2019-03-31 17:57:23 +0300

    1.3.4 acces to DOM elemnts

--- src/app/add-car/add-car.component.ts
+++ src/app/add-car/add-car.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    EventEmitter,
+    Output,
+    ViewChild,
+    ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'app-add-car',
@@ -10,19 +17,20 @@ export class AddCarComponent implements OnInit {
   carName = '';
   carYear = 2017;
   @Output('onAddCar') carEmitter = new EventEmitter<{name: string, year: number}>();
+  @ViewChild('carYearInput') carYearInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  addCar() {
+  addCar(carNameEl: HTMLInputElement) {
     this.carEmitter.emit({
-        name: this.carName,
-        year: this.carYear
+        name: carNameEl.value,
+        year: +this.carYearInput.nativeElement.value
     });
 
-    this.carName = '';
-    this.carYear = 2017;
+    carNameEl.value = '';
+    this.carYearInput.nativeElement.value = 2017;
   }
 }
