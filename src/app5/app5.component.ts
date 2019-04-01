46038640dbf5cd972ebc59a302c78d3cad43e806
tral909 <indorm1992@mail.ru>
2019-04-01 22:32:01 +0300

    1.5.1 pipe using. end

--- src/app5/app5.component.ts
+++ src/app5/app5.component.ts
@@ -2,10 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //https://angular.io/docs/ts/latest/api/common/index/DatePipe-pipe.html
   template: `
-    <div class="col-xs-8 col-xs-offset-2">
-      
+  <div class="container">
+    <div class="col-8 col-offset-2">
+      <h5>{{ name }}</h5>
+      <h5>{{ name | lowercase }}</h5>
+      <h5>{{ name | uppercase }}</h5>
+      <h5>{{ name | slice:0:3 }}</h5>
+      <h5>{{ name | slice:0:3 | uppercase }}</h5>
+      <hr>
+      <h5>{{ pi }}</h5>
+      <h5>{{ pi | number }}</h5>
+      <h5>{{ pi | number:'1.3-4' }}</h5>
+      <hr>
+      <h5>{{ money | currency:'EUR' }}</h5>
+      <hr>
+      <h5>{{ date }}</h5>
+      <h5>{{ date | date }}</h5>
+      <h5>{{ date | date:'fullDate' }}</h5>
+      <h5>{{ date | date:'shortDate' }}</h5>
+      <h5>{{ date | date:'shortTime' }}</h5>
+      <hr>
+      <h5>{{ amount }}</h5>
+      <h5>{{ amount | percent }}</h5>
+      <hr>
+      <h5>{{ object }}</h5>
+      <h5><pre>{{ object | json }}</pre></h5>
     </div>
+  </div>
   `
 })
 export class AppComponent {
