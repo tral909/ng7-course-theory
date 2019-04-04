a220a4ee30047872f78fd7968af1e62d0f18e2c1
tral909 <indorm1992@mail.ru>
2019-04-05 00:07:00 +0300

    1.7.11 Reactive create validator

--- src/app7.8-17.2/app7.8-17.2.component.ts
+++ src/app7.8-17.2/app7.8-17.2.component.ts
@@ -16,13 +16,15 @@ export class AppComponent implements OnInit {
     text: 'Нет'
   }];
 
+  charsCount = 5;
+
   form: FormGroup;
 
   ngOnInit() {
   	this.form = new FormGroup({
       user: new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
-        pass: new FormControl('', Validators.required),
+        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
       }),
   		country: new FormControl('ru'),
   		answer: new FormControl('no')
@@ -31,5 +33,16 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
   	console.log('Submited!', this.form);
+  }
+
+  // {'errorCode': true}
+  // null undefined
+  checkForLength(control: FormControl) {
+    if (control.value.length <= this.charsCount) {
+      return {
+        'lengthError': true
+      };
+    }
+    return null;
   }
 }
