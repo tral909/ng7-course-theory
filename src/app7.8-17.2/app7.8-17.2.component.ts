513820b7220902496cdf4c3d55a91d6edf4a81e1
tral909 <indorm1992@mail.ru>
2019-04-05 00:31:12 +0300

    1.7.12 Reactive async validator

--- src/app7.8-17.2/app7.8-17.2.component.ts
+++ src/app7.8-17.2/app7.8-17.2.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
   	this.form = new FormGroup({
       user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
+        email: new FormControl('', [Validators.required, Validators.email], this.checkForEmail),
         pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
       }),
   		country: new FormControl('ru'),
@@ -44,5 +44,19 @@ export class AppComponent implements OnInit {
       };
     }
     return null;
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@mail.ru') {
+          resolve({
+            'emailIsUsed': true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
   }
 }
