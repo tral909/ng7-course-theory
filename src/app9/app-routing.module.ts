cce872db9034f20dc06c303e33b47edfc51c5d11
tral909 <indorm1992@mail.ru>
2019-04-08 00:35:27 +0300

    1.9.5 params of address line. lesson 1

--- src/app9/app-routing.module.ts
+++ src/app9/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
+import { CarPageComponent } from './car-page/car-page.component';
 
 const appRoutes: Routes = [
-  // localhost:4200/cars
   { path: 'cars', component: CarsPageComponent },
+  { path: 'cars/:id/:name', component: CarPageComponent },
   { path: '', component: HomePageComponent }
 ];
 
