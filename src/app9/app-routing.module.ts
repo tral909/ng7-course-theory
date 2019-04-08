7dfb89f9943822fa966d8b50dc130506956c1a9d
tral909 <indorm1992@mail.ru>
2019-04-08 23:10:33 +0300

    1.9.7 embedded routes

--- src/app9/app-routing.module.ts
+++ src/app9/app-routing.module.ts
@@ -6,8 +6,9 @@ import { CarsPageComponent } from './cars-page/cars-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
 
 const appRoutes: Routes = [
-  { path: 'cars', component: CarsPageComponent },
-  { path: 'cars/:id/:name', component: CarPageComponent },
+  { path: 'cars', component: CarsPageComponent, children: [
+    { path: ':id/:name', component: CarPageComponent }
+  ]},
   { path: '', component: HomePageComponent }
 ];
 
