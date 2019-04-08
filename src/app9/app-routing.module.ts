11743d73548d0a8f96de4dcf86ba2afd887875e0
tral909 <indorm1992@mail.ru>
2019-04-09 00:08:57 +0300

    1.9.8 redirect and error handling

--- src/app9/app-routing.module.ts
+++ src/app9/app-routing.module.ts
@@ -4,12 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
 import { CarPageComponent } from './car-page/car-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   { path: 'cars', component: CarsPageComponent, children: [
     { path: ':id/:name', component: CarPageComponent }
   ]},
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found' }
 ];
 
 
