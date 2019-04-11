4dfc2dc54bb6f6b084c0f7d9b0cf92d927a96ceb
tral909 <indorm1992@mail.ru>
2019-04-11 23:44:35 +0300

    1.12.1 launching tests

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app10/app10.module';
+import { AppModule } from './app12/app12.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
