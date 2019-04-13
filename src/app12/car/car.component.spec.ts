2e9062d0c564d0fdc71657291a33361a05213445
tral909 <indorm1992@mail.ru>
2019-04-13 17:41:06 +0300

    1.12.3 service testing. injection

--- src/app12/car/car.component.spec.ts
+++ src/app12/car/car.component.spec.ts
@@ -1,25 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 
 import { CarComponent } from './car.component';
+import { CarService } from './car.service';
   
 describe('CarComponent', () => {
+    let fixture: ComponentFixture<CarComponent>;
+    let component: CarComponent;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [CarComponent]
         });
+
+        fixture = TestBed.createComponent(CarComponent);
+        component = fixture.debugElement.componentInstance;
     });
 
     it('should create component instance', () => {
-        const fixture = TestBed.createComponent(CarComponent);
-        const component = fixture.debugElement.componentInstance;
         expect(component).toBeTruthy();
     });
 
     it(`should render h1 tag with title 'My car header'`, () => {
-        const fixture = TestBed.createComponent(CarComponent);
         fixture.detectChanges();
-        const component = fixture.debugElement.nativeElement;
-        const text = component.querySelector('h1').textContent;
+        const nativeEl = fixture.debugElement.nativeElement;
+        const text = nativeEl.querySelector('h1').textContent;
         expect(text).toEqual('My car header');
+    });
+
+    it(`it should inject CarService`, () => {
+        const carService = fixture.debugElement.injector.get(CarService);
+        fixture.detectChanges();
+        expect(component.isCarVisible).toEqual(carService.getVisibility());
     });
 });
--- src/app12/car/car.component.ts
+++ src/app12/car/car.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CarService } from './car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
+  providers: [CarService]
 })
 export class CarComponent implements OnInit {
 
-  isCarVisible = true;
+  isCarVisible: boolean;
   title = 'My car header';
 
-  constructor() { }
+  constructor(private carService: CarService) { }
 
   ngOnInit() {
+      this.isCarVisible = this.carService.getVisibility();
   }
 
 }
